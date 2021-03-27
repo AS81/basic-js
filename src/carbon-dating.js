1475490a7e7deaf7ff4fb1b006ed88e053439160
AS81 <sanloginchik@gmail.com>
2021-03-27 23:19:48 +0300

    init: task basic-js and solve count-cats & carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
   // remove line with error and write your code here
+  if (
+    /[0-9]/.test(sampleActivity) &&
+    sampleActivity > 0 &&
+    sampleActivity < MODERN_ACTIVITY &&
+    typeof sampleActivity == "string"
+  ) {
+    const nLog2 = 0.693;
+
+    return Math.ceil(
+      Math.log(MODERN_ACTIVITY / +sampleActivity) / (nLog2 / HALF_LIFE_PERIOD)
+    );
+  } else return false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(backyard) {
   // remove line with error and write your code here
+  let cats = 0;
+  backyard.forEach((row) => {
+    row.forEach((elem) => {
+      if (elem === "^^") {
+        cats++;
+      }
+    });
+  });
+  return cats;
 };
