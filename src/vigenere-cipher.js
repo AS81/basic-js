e1cbe7855aaf770e73f8f0eea3e129a74c025535
AS81 <sanloginchik@gmail.com>
2021-03-30 02:14:14 +0300

    feat: end the task vigenere-cipher 99%

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,60 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
+  constructor(direct = true) {
+    this.direction = direct;
+  }
+  encrypt(message, key) {  
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    if (!message || !key) throw new Error('Please, give message and key');
+    message = (message).toUpperCase();
+    key = (key).toUpperCase();
+    const arrM = message.split('');
+    const ABClen = 26;
+    const arrEnc = [];
+    let newSymb;
+    let i = 0;
+    let summOfCode;
+  
+    arrM.forEach((symb) => {
+      let symbChar = symb.charCodeAt(0);
+      if (symbChar >= 65 && symbChar <= 90) {
+        summOfCode = symbChar-65 + (key.charCodeAt(i)-65);
+        if (summOfCode >= ABClen) summOfCode -= ABClen
+        arrEnc.push(String.fromCharCode(summOfCode + 65));      
+        if (i != key.length-1) i++
+        else i = 0
+      } else arrEnc.push(symb);
+    })
+    if (this.direct = false) {arrEnc.reverse()}
+    return arrEnc.join('');
+  } 
+      
+  decrypt(messageEnc, key) {
+  if (!messageEnc || !key) throw new Error('Please, give enc message and key');
+  messageEnc = (messageEnc).toUpperCase();
+  key = (key).toUpperCase();
+  const arrMEnc = messageEnc.split('');
+  const ABClen = 26;
+  const arr = [];
+  let sourceSymb;
+  let i = 0;
+  let diffOfCode;
+
+  arrMEnc.forEach((symb) => {
+    let symbChar = symb.charCodeAt(0);
+    if (symbChar >= 65 && symbChar <= 90) {
+      diffOfCode = symbChar-65 - (key.charCodeAt(i)-65);
+      if (diffOfCode < 0) diffOfCode += ABClen;
+      arr.push(String.fromCharCode(diffOfCode + 65));      
+      if (i != key.length-1) i++
+      else i = 0
+    } else arr.push(symb);
+  })
+  if (this.direct = false) {arr.reverse()}
+  return arr.join('');
   }
 }
 
