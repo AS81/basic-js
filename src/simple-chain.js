39a8e92acd30749a693f7c4a138afb8e5919ac7e
AS81 <sanloginchik@gmail.com>
2021-03-30 21:58:37 +0300

    feat: end the task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+    arr: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '( )') {
+    this.arr.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if ((+position === position) && Number.isInteger(position)
+    && (this.arr[position] != undefined)) {  
+      this.arr.splice(position-1,1);
+      return this;
+    } else {
+      this.arr = [];
+      throw new Error('Please, give a correct number for removeLink() !')
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.strFinish = this.arr.join('~~');
+    this.arr = [];
+    return this.strFinish;
   }
 };
 
